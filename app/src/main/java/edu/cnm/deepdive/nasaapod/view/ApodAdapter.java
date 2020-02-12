add35767e311da6c39a4cc70d9cee91d400f263f
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-02-12 10:26:05 -0700

    Refactor loading to activity (in progress)

--- app/src/main/java/edu/cnm/deepdive/nasaapod/view/ApodAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/view/ApodAdapter.java
@@ -12,22 +12,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.squareup.picasso.Picasso;
 import edu.cnm.deepdive.nasaapod.R;
+import edu.cnm.deepdive.nasaapod.model.entity.Apod;
 import edu.cnm.deepdive.nasaapod.model.entity.Apod.MediaType;
 import edu.cnm.deepdive.nasaapod.model.pojo.ApodWithStats;
 import java.util.List;
 
 public class ApodAdapter extends ArrayAdapter<ApodWithStats> {
 
-  public ApodAdapter(@NonNull Context context,
-      @NonNull List<ApodWithStats> objects) {
-    super(context, R.layout.item_apod, objects);
+  public ApodAdapter(@NonNull Context context, @NonNull List<ApodWithStats> apods) {
+    super(context, R.layout.item_apod, apods);
   }
 
   @NonNull
   @Override
   public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
     View view = convertView;
-    if(view == null) {
+    if (view == null) {
       view = LayoutInflater.from(getContext()).inflate(R.layout.item_apod, parent, false);
     }
     ImageView thumbnail = view.findViewById(R.id.thumbnail);
@@ -37,13 +37,22 @@ public class ApodAdapter extends ArrayAdapter<ApodWithStats> {
     ApodWithStats apod = getItem(position);
     title.setText(apod.getApod().getTitle());
     date.setText(DateFormat.getMediumDateFormat(getContext()).format(apod.getApod().getDate()));
-    access.setText(getContext().getString(R.string.access_foramt, apod.getAccessCount(),
-        DateFormat.getMediumDateFormat(getContext()).format(apod.getLastAccess())));
-    if (apod.getApod().getMediaType() == MediaType.IMAGE){
+    String countQuantity = getContext().getResources()
+        .getQuantityString(R.plurals.access_count, apod.getAccessCount());
+    access.setText(getContext().getString(R.string.access_format, apod.getAccessCount(),
+        DateFormat.getMediumDateFormat(getContext()).format(apod.getLastAccess()), countQuantity));
+    if (apod.getApod().getMediaType() == MediaType.IMAGE) {
       Picasso.get().load(apod.getApod().getUrl()).into(thumbnail);
     } else {
       thumbnail.setImageResource(R.drawable.ic_slow_motion_video);
     }
     return view;
   }
+
+  public interface OnClickListener {
+
+    void onClick(View v, Apod apod, int position);
+
+  }
+
 }
